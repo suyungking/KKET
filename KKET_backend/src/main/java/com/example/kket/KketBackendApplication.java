53f5dbb95dbe65b30eb6cddaf126bbc9787a221a
suyungking <suyungking@naver.com>
2026-01-08 11:40:14 +0900

    feat : 프로젝트 생성

---
+++ KKET_backend/src/main/java/com/example/kket/KketBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.kket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KketBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KketBackendApplication.class, args);
+	}
+
+}
---
+++ KKET_backend/src/test/java/com/example/kket/KketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KketBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
